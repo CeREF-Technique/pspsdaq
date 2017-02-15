931716949db6b03307a86fdb1d9c5c40a7660690
MaxDum <maxim.dumortier@cerisic.be>
2017-02-15 11:44:36 +0100

    Update export file with excel format
    The export is now able to write the data in xlsx format

--- lib/export/testExport.py
+++ lib/export/testExport.py
@@ -5,5 +5,13 @@ stuff = Export("titi", header=["yeah","great","such","header"])
 stuff.writerow(["bla","tutu","truc"])
 stuff.writerow(["bla2","tutu2","truc2"])
 
+another2Stuff = Export("toto", type="xlsx",header=["t","u","w"])
+another2Stuff.writerow([1,2,3])
+another2Stuff.writerow([4,5,6])
+another2Stuff.writerow([7,8,9])
+
+another3Stuff = Export("toto2", type="xml",header=["t","u","w"])
+another3Stuff.writerow([1,2,3])
+
 anotherStuff = Export("titi2", type="bla")
 anotherStuff.writerow([1,2,3])
