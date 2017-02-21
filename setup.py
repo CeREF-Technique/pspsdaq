7c9dd7527eb4cc674380650360e3542a7501cbf1
MaxDum <maxim.dumortier@cerisic.be>
2017-02-21 17:00:59 +0100

    Update Icon and setup version
    Update the Icone with a own creation one
    Updated the setup file de to some trouble to freeze the code with
    openpyxl. Problem solved in the openpyxl folder (see wiki)

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from cx_Freeze import setup, Executable  # cw_Freeze needs to be installed, cmd : pip install cx_freeze
 import sys
+import os
 
 __author__ = 'Maxim Dumortier'
 
@@ -9,23 +10,32 @@ It allows us to make some .exe files or .msi files
 To execute it, use the Create_exe.bat and Create_msi.bat files.
 """
 
+
 build_exe_options = {"packages": ["os"]}
 
 base = None
 if sys.platform.startswith('win'):
     base = "Win32GUI"
 
-icon = "icon.ico"  # Desktop icon
+icon = "PS2DAq.ico"  # Desktop icon
 
 setup(
     name = "(PS)²DAq",
 
-    version = "0.1b",
+    version = "0.2b",
+    
+    author = "Cerisic ASBL",
 
     description = "Python Serial Power Supply Data Acquisition - (PS)²DAq ; Graphical interface to communicate with a power supply in order to acquire some data",
 
     options = {"build_exe": build_exe_options},
 
-    executables=[Executable("main.py", base=base, shortcutName="(PS)²DAq", shortcutDir="DesktopFolder",
+    executables=[Executable("main.py",
+                            base=base,
+                            targetName = "ps2daq.exe",
+                            compress = False,
+                            shortcutName="(PS)²DAq",
+                            shortcutDir="DesktopFolder",
+                            copyDependentFiles = True,
                             icon=icon)],
-)
+    )
