32335b96f17179b11abfd2c7bfad7b292d6935ab
MaxDum <maxim.dumortier@cerisic.be>
2017-05-29 17:00:15 +0200

    Some cleanings

--- main.py
+++ main.py
@@ -19,18 +19,19 @@ __author__ = 'Maxim Dumortier'
     CERISIC, Mons, BELGIUM
 
     The Goal of this code is to talk with a Power supply in serial mode (COM port) and to ask with polling the different kind of measures.
-    At the end, it will write a Excell file with all the values (absolute time, relative time, Voltage, current, power)
+    At the end, it will write a Excel file with all the values (absolute time, relative time, Voltage, current, power)
 """
 
 # Global vars : 
-ICON_PATH = "./res/PS2DAq.ico" # Path to the PS2Daq icon
-flagStartStop = False # Flag for the Start/Stop button
-properties = {} # dict of all the properties used in this code
-file_type = "XLSX" # default file type
+ICON_PATH = "./res/PS2DAq.ico"  # Path to the PS2Daq icon
+flagStartStop = False  # Flag for the Start/Stop button
+properties = {}  # dict of all the properties used in this code
+file_type = "XLSX"  # default file type
 
 #
 # FUNCTIONS :
 #
+
 
 def connect_serial_port():
     """
@@ -43,14 +44,14 @@ def connect_serial_port():
         global ps # power supply
         global thread_stop
 
-        ps = PS_name_dict[psChoice.get()](portChoice.get()) # Instantiate the choosen Power Supply 
+        ps = PS_name_dict[psChoice.get()](portChoice.get())  # Instantiate the chosen Power Supply
         
         connectButton.config(state="disabled")  # change the stat of the connect button to disabled
         disconnectButton.config(state="normal")  # change the stat of the disconnect button to enabled
         portComboBox.config(state="disabled")
         psCombobox.config(state="disabled")
         startStopButton.config(state="normal")
-        deviceID.set("Serial number : " + ps.getID()) # Show the Device ID
+        deviceID.set("Serial number : " + ps.getID())  # Show the Device ID
         logging.info("Connected to de device " + deviceID.get())
         
         thread_stop = Event()  # defines a new thread stop for every connection to serial port
@@ -58,7 +59,7 @@ def connect_serial_port():
         def addMeasure(inputDict, rowNbr):
             """
             Add a measure to the GUI
-            :param inputDict: dictionnary of the available measures for this PS
+            :param inputDict: dictionary of the available measures for this PS
             :param rowNbr: number of the row in the GUI grid
             :return: nothing
             """
@@ -72,7 +73,7 @@ def connect_serial_port():
         startRowNbr = 3
         rowIndex = 0
         for meas in sorted(ps.availableMeasures.keys()):
-            if ps.availableMeasures[meas]["used"]: # show only if it's used
+            if ps.availableMeasures[meas]["used"]:  # show only if it's used
                 addMeasure(ps.availableMeasures[meas], startRowNbr + rowIndex)
                 rowIndex += 1
         
@@ -91,7 +92,7 @@ def disconnect_serial_port():
             portComboBox.config(state="normal")
             startStopButton.config(state="disabled")
             psCombobox.config(state="normal")
-            menubar.entryconfig("Configure",state="normal")
+            menubar.entryconfig("Configure", state="normal")
         except:
             pass
         ps.ser.close()
@@ -120,7 +121,6 @@ def goodbye():
     logging.info("Window was closed brutally")
 
 
-
 def start_mesure():
     """
     Start measuring
@@ -130,36 +130,36 @@ def start_mesure():
     global file_type
     global exp
     
-    if len(sampleEntry.get()) != 0: # First check that there's something in the textbox
+    if len(sampleEntry.get()) != 0:  # First check that there's something in the textbox
         # remove some text if it is shown :
-        sampleEntry.set(sampleEntry.get().replace("Too short interval : ",""))
-        sampleEntry.set(sampleEntry.get().replace(" sec",""))
+        sampleEntry.set(sampleEntry.get().replace("Too short interval : ", ""))
+        sampleEntry.set(sampleEntry.get().replace(" sec", ""))
         # In case the user put a coma :
-        sampleEntry.set(sampleEntry.get().replace(",","."))
+        sampleEntry.set(sampleEntry.get().replace(",", "."))
         
         if isfloat(sampleEntry.get()): # Second, check if the entry is a float
             sampleTime = float(sampleEntry.get())
-            # Convert the sampletime setted into seconds
+            # Convert the sample time set into seconds
             if timeUnitCombobox.get() == "minute(s)":
                 sampleTime *= 60
             elif timeUnitCombobox.get() == "hour(s)":
                 sampleTime *= 3600
             # Else it is seconds, so no need to convert
                 
-            if sampleTime > 0: # Third, check that the entry is a positive time (no signification of a negative time)
+            if sampleTime > 0:  # Third, check that the entry is a positive time (no signification of a negative time)
                 
-                if flagStartStop: # The User clicked on "STOP"
-                    thread_stop.set() # Stop the read thread
+                if flagStartStop:  # The User clicked on "STOP"
+                    thread_stop.set()  # Stop the read thread
                     
                     startStopButton.config(text="Start")
                     flagStartStop = False
                     
-                    sampleTimeEntryBox.config(state="normal") # lock the entry and time unit during measurements
+                    sampleTimeEntryBox.config(state="normal")  # lock the entry and time unit during measurements
                     timeUnitCombobox.config(state="normal")
                     menubar.entryconfig("Configure", state="normal")
                     
                     logging.info("Stopped measurement")
-                else: # The User clicked on "START"
+                else:  # The User clicked on "START"
                     beginTime = time.time()
                     next_call = time.time()
                     if "default.file.location" in properties:
@@ -174,26 +174,26 @@ def start_mesure():
                             fileName = "PowerSupplyData-" + time.strftime("%Y%m%d-%H%M%S")
                     else:
                         fileName = "PowerSupplyData-" + time.strftime("%Y%m%d-%H%M%S")
-                    mesure_number = 1 # Initialize de measure number
+                    mesure_number = 1  # Initialize de measure number
                     header = ["Measure Number", "Local time", "Relative time (s)"]
                     for meas in sorted(ps.availableMeasures.keys()):
-                        if ps.availableMeasures[meas]["used"]: # show only if it's used
+                        if ps.availableMeasures[meas]["used"]:  # show only if it's used
                             header.append(ps.availableMeasures[meas]["label"] + " ("
                                           + ps.availableMeasures[meas]["units"] + ")")
                     # Initialize the export class (Excel and CSV)
                     exp = Export(fileName, file_type=file_type, header=header)
                     
-                    if thread_stop.isSet(): # Reset the stop Event
+                    if thread_stop.isSet():  # Reset the stop Event
                         thread_stop.clear()
                         
-                    read(sampleTime, beginTime, next_call, thread_stop) # Launch the read thread
+                    read(sampleTime, beginTime, next_call, thread_stop)  # Launch the read thread
                         
                     startStopButton.config(text="Stop")
                     flagStartStop = True
 
                     sampleTimeEntryBox.config(state="disabled")
                     timeUnitCombobox.config(state="disabled")
-                    menubar.entryconfig("Configure",state="disabled")
+                    menubar.entryconfig("Configure", state="disabled")
                     
                     logging.info("Started measurement with %d sec. as sample time", sampleTime)
                     
@@ -225,25 +225,23 @@ def read(interval, beginTime, next_call, stop_event):
         deltaTime = '%.1f' % round(currentTime-beginTime, 1)
         toWrite = [mesure_number, time.strftime("%Y/%m/%d-%H:%M:%S"), float(deltaTime)]
         for meas in sorted(ps.availableMeasures.keys()):
-            if ps.availableMeasures[meas]["used"]: # show only if it's used
+            if ps.availableMeasures[meas]["used"]:  # show only if it's used
                 ps.availableMeasures[meas]["stringVar"].set(ps.availableMeasures[meas]["format"]
                                                             % ps.availableMeasures[meas]["method"]())
                 
-                if "f" in ps.availableMeasures[meas]["format"]: # Float format
+                if "f" in ps.availableMeasures[meas]["format"]:  # Float format
                     toWrite.append(float(ps.availableMeasures[meas]["stringVar"].get()))
-                elif "s" in ps.availableMeasures[meas]["format"]: # string format
+                elif "s" in ps.availableMeasures[meas]["format"]:  # string format
                     toWrite.append(ps.availableMeasures[meas]["stringVar"].get())
-                elif "d" in ps.availableMeasures[meas]["format"]: # int format
+                elif "d" in ps.availableMeasures[meas]["format"]:  # int format
                     toWrite.append(int(ps.availableMeasures[meas]["stringVar"].get()))
                 else:
                     toWrite.append(ps.availableMeasures[meas]["stringVar"].get())
-        
-        
+
         exp.writerow(toWrite)
         mesure_number += 1
     else:
         logging.error("Read Broke down")
-
 
     if not stop_event.is_set():
         next_call += interval
@@ -252,7 +250,7 @@ def read(interval, beginTime, next_call, stop_event):
             # must adjust it to a higher value
             interval -= next_call - time.time()
             sampleEntry.set("Too short interval : " + "%.3f" % interval + " sec")
-        Timer(next_call - time.time(),read,[interval, beginTime, next_call, stop_event]).start()
+        Timer(next_call - time.time(), read, [interval, beginTime, next_call, stop_event]).start()
              
 
 def isfloat(strin):
@@ -262,7 +260,7 @@ def isfloat(strin):
     :return: True if the parameter can be converted into a float, False otherwise
     """
     try:
-        strin = strin.replace(",",".")
+        strin = strin.replace(",", ".")
         float(strin)
         return True
     except ValueError:
@@ -274,9 +272,9 @@ def isfloat(strin):
 #
 
 logging.basicConfig(filename='PS2DAq.log', format='%(levelname)s\t%(asctime)s\t%(message)s', level=logging.DEBUG)
-logging.info("Application Starded")
+logging.info("Application Started")
 
-properties = readProperties() # store the properties from the file
+properties = readProperties()  # store the properties from the file
 
 
 root = tk.Tk()  # create a new window
@@ -284,7 +282,8 @@ root.geometry(properties["default.windowSize"])  # set the size of the window
 root.title("Python Serial Power Supply Data Acquisition - (PS)²DAq")  # set a title to the window
 tk.Label(root, text="Software to read and monitor data of a power supply through USB-serial port")\
     .grid(column=0, row=0, columnspan=4, padx=5, pady=5)  # little explanation of what the window can do
-root.iconbitmap(default=ICON_PATH) # set the beatiful icon on the app
+root.iconbitmap(default=ICON_PATH)  # set the beautiful icon on the app
+
 
 def getAvailableSerialPorts(cmBox, connBtn):
     """
@@ -321,7 +320,7 @@ def getAvailableSerialPorts(cmBox, connBtn):
         logging.info("No Serial Port Available")
     else:
         connBtn.config(state="normal")
-        logging.debug("Found Serialports : %s", result)
+        logging.debug("Found Serial ports : %s", result)
         
     cmBox["values"] = result  # Set the list of the serial ports
     
@@ -338,11 +337,12 @@ portChoice = tk.StringVar()
 portComboBox = ttk.Combobox(root, textvariable=portChoice)
 getAvailableSerialPorts(portComboBox, connectButton)
 portContextMenu = tk.Menu(root, tearoff=0) # create a context menu to be able to refresh the serial ports
-portContextMenu.add_command(label="Refrech Ports", command=lambda : getAvailableSerialPorts(portComboBox, connectButton))
+portContextMenu.add_command(label="Refrech Ports", command=lambda: getAvailableSerialPorts(portComboBox, connectButton))
+
 
 def portContextMenuPopup(event):
     """
-    Function which show a contextmenu when a right click occures on the Serial port combobox, in order to refresh the port lst
+    Function which show a context menu when a right click occurs on the Serial port combobox, in order to refresh the port list
     :param event: source event
     :return: nothing
     """
@@ -352,14 +352,14 @@ def portContextMenuPopup(event):
 
 # attach popup to frame
 portComboBox.bind("<Button-3>", portContextMenuPopup)
-portComboBox.grid(column=0,row=1, padx=5, pady=5)  # set the combobox at the right place
+portComboBox.grid(column=0, row=1, padx=5, pady=5)  # set the combobox at the right place
 
 # Disconnection Button
 disconnectButton = tk.Button(root, text="Disconnect...", state="disabled", command=disconnect_serial_port)
 disconnectButton.grid(column=2, row=1, padx=5, pady=5)
 deviceID = tk.StringVar()
 deviceID.set("")
-tk.Label(root,textvariable=deviceID).grid(column=3, row=1, padx=5, pady=5)
+tk.Label(root, textvariable=deviceID).grid(column=3, row=1, padx=5, pady=5)
 
 
 #
@@ -373,7 +373,9 @@ sampleTimeEntryBox.grid(column=1, row=2, padx=5, pady=5, sticky=tk.N + tk.E + tk
 sampleEntry.set(properties["default.timeValue"])
 
 psChoice = tk.StringVar()
-dynamicGUIlist=[] # list of the dynamic GUI objects, used to remove them from the GUI when the user switch of PS
+dynamicGUIlist=[]  # list of the dynamic GUI objects, used to remove them from the GUI when the user switch of PS
+
+
 def updatedCombobox(index, value, op):
     """
     Function called each time the user change the combobox value
@@ -383,7 +385,7 @@ def updatedCombobox(index, value, op):
     :return: nothing
     """
     global dynamicGUIlist
-    #print("combobox updated to " + psCombobox.get())
+    # print("combobox updated to " + psCombobox.get())
     try:
         for obj in dynamicGUIlist:
             obj.grid_forget()
@@ -395,15 +397,14 @@ def updatedCombobox(index, value, op):
     def addMeasure(inputDict, rowNbr):
         """
         Add a measure to the GUI
-        :param inputDict: dictionnary of the available measures for this PS
+        :param inputDict: dictionary of the available measures for this PS
         :param rowNbr: number of the row in the GUI grid
         :return: nothing
         """
         l1 = tk.Label(root, text=inputDict["label"])
         l1.grid(column=0, row=rowNbr, padx=5, pady=5)
         l2 = tk.Label(root, textvariable=inputDict["stringVar"], relief=tk.SOLID)
-        l2.grid(column=1, row=rowNbr, padx=5, pady=5,
-                                                              sticky=tk.N + tk.E + tk.S + tk.W)
+        l2.grid(column=1, row=rowNbr, padx=5, pady=5, sticky=tk.N + tk.E + tk.S + tk.W)
         inputDict["stringVar"].set("###")
         l3 = tk.Label(root, text=inputDict["units"])
         l3.grid(column=2, row=rowNbr, padx=5, pady=5)
@@ -414,19 +415,19 @@ def updatedCombobox(index, value, op):
 
     startRowNbr = 3
     rowIndex = 0
-    ps = PS_name_dict[psChoice.get()]("") # Instantiate the choosen Power Supply 
+    ps = PS_name_dict[psChoice.get()]("")  # Instantiate the chosen Power Supply
     for meas in sorted(ps.availableMeasures.keys()):
-        if ps.availableMeasures[meas]["used"]: # show only if it's used
+        if ps.availableMeasures[meas]["used"]:  # show only if it's used
             addMeasure(ps.availableMeasures[meas], startRowNbr + rowIndex)
             rowIndex += 1
-    #print(dynamicGUIlist)
+    # print(dynamicGUIlist)
 
 psChoice.trace('w', updatedCombobox)
 psCombobox = ttk.Combobox(root, textvariable=psChoice)
 
 PS_name_dict = {}
 for s in PS.__all__:
-    PS_name_dict[(PS.classDict[s].name)] = PS.classDict[s]
+    PS_name_dict[PS.classDict[s].name] = PS.classDict[s]
 
 psCombobox["values"] = list(PS_name_dict.keys())
 psChoice.set(properties["default.powerSupply"])
@@ -434,18 +435,19 @@ psCombobox.grid(column=4, row=1, padx=5, pady=5)
 
 timeUnitChoice = tk.StringVar()
 timeUnitCombobox = ttk.Combobox(root, textvariable=timeUnitChoice)
-timeUnitCombobox["values"] = ["second(s)","minute(s)","hour(s)"]
+timeUnitCombobox["values"] = ["second(s)", "minute(s)", "hour(s)"]
 timeUnitChoice.set(properties["default.timeUnit"])
-timeUnitCombobox.grid(column=2,row=2, padx=5, pady=5)
+timeUnitCombobox.grid(column=2, row=2, padx=5, pady=5)
 
 startStopButton = tk.Button(root, text="Start", command=start_mesure, state="disabled")
 startStopButton.grid(column=3, row=2, padx=5, pady=5)
+
 
 def hello():
     print("hello")
 menubar = tk.Menu(root)
 
-# create a pulldown menu, and add it to the menu bar
+# create a pull down menu, and add it to the menu bar
 filemenu = tk.Menu(menubar, tearoff=0)
 filemenu.add_command(label="Open", command=hello)
 filemenu.add_command(label="Save", command=hello)
@@ -453,10 +455,11 @@ filemenu.add_separator()
 filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
-# create more pulldown menus
+# create more pull down menus
 configmenu = tk.Menu(menubar, tearoff=0)
 
-def chooseOutputType(choice): # set the file_type to the selected choice
+
+def chooseOutputType(choice):  # set the file_type to the selected choice
     """
     Put the selected choice into the file type and the properties file
     :param choice: choice made (CSV or XLSX)
@@ -477,7 +480,7 @@ for key in sorted(Export.availableExport().keys()):
     val = Export.availableExport()[key]
     if file_type.lower() == val.lower():
         v.set(key)
-    configOutputFileMenu.add_radiobutton(label=key,variable=v, command=lambda val=val: chooseOutputType(val))
+    configOutputFileMenu.add_radiobutton(label=key, variable=v, command=lambda val=val: chooseOutputType(val))
 
 configDataToSave = tk.Menu(configmenu, tearoff=0)
 
@@ -488,7 +491,7 @@ def chooseOutputFilePath():
     :return: nothing
     """
     folder = tk.filedialog.askdirectory()
-    properties["default.file.location"]=folder
+    properties["default.file.location"] = folder
     writeProperties(properties)
     
 configmenu.add_cascade(label="Output File Type", menu=configOutputFileMenu)
